6604a52aa98d68c9d87fb65e7e8ca61d2a14d570
Kamalani DeFreitas <109827214+kdefreitas1@users.noreply.github.com>
2025-10-01 14:15:46 -0400

    Updated getEvents and displayEvents
    getEvents now passes data to displayEvents to display the data.

--- front-page/index.js
+++ front-page/index.js
@@ -1,16 +1,16 @@
-async function getEvents(){
-    try{
-        const response = await fetch('http://localhost:3000/api/events');
-        const data = await response.json();
-        return data;
-    }catch(error){
-        console.error(error);
-    }
-}
-
 function displayEvents(data){
     const names = data.map(event => event.name);
     console.log(names);
 }
 
-displayEvents(getEvents());
+async function getEvents(){
+    try{
+        const response = await fetch('http://localhost:3000/api/events');
+        const data = await response.json();
+        displayEvents(data);
+    }catch(error){
+        console.error(error);
+    }
+}
+
+getEvents();
