e1e57ba200071545a877f4f069aaa9540f02e99d
Kamalani DeFreitas <109827214+kdefreitas1@users.noreply.github.com>
2025-10-12 15:07:02 -0400

    Updated to call API every 24 hours
    When refreshing page no longer recalls API and uses the key.

--- backend/server.js
+++ backend/server.js
@@ -2,19 +2,23 @@ require('dotenv').config('./backend/.env');
 
 const express = require('express');
 const cors = require('cors');
+const cron = require('node-cron');
 const app = express();
 
 app.use(cors());
 
 const ticketmasterApiKey = process.env.TICKETMASTER_API_KEY;
 
-app.get('/api/events', async (req, res) => {
+let events = [];
+let lastUpdated = null;
+
+async function fetchEvents() {
     try {
         const apiUrl = `https://app.ticketmaster.com/discovery/v2/events.json?geoPoint=dp6tj&radius=30&unit=miles&size=100&sort=date,asc&apikey=${ticketmasterApiKey}`;
         const response = await fetch(apiUrl);
         const data = await response.json();
 
-        const events = data._embedded.events.map(event => ({
+        events = data._embedded.events.map(event => ({
             name: event.name,
             date: event.dates.start.localDate,
             eventType: event.classifications?.[0]?.segment?.name,
@@ -25,11 +29,19 @@ app.get('/api/events', async (req, res) => {
             city: event._embedded?.venues?.[0]?.city?.name,
         }));
 
-        res.json(events);
+        lastUpdated = new Date();
     } catch (error) {
         console.error('Error fetching events:', error);
         res.status(500).send('Error fetching events');
     }
+}
+
+fetchEvents();
+cron.schedule('0 */24 * * *', fetchEvents);
+
+app.get('/api/events', (req, res) => {
+    console.log(`Last updated: ${lastUpdated}`);
+    res.json(events);
 });
 
 app.listen(3000, () => {
