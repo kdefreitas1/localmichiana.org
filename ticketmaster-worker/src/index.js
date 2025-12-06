15f1c720a76b54315c4e750586822f5d0101b638
Kamalani DeFreitas <109827214+kdefreitas1@users.noreply.github.com>
2025-12-05 21:25:35 -0500

    Update for CORS

--- ticketmaster-worker/src/index.js
+++ ticketmaster-worker/src/index.js
@@ -6,6 +6,13 @@ const app = express();
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*"); // <-- or "https://yourfrontend.com"
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 const ticketmasterApiKey = env.TICKETMASTER_API_KEY;
 
 let events = [];
