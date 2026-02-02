2f133aa9a5cc400b17e51bc5e7a8da1f5307e1e2
Kamalani DeFreitas <109827214+kdefreitas1@users.noreply.github.com>
2026-02-01 21:59:25 -0500

    Updating stuff and things

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ const app = express();
 app.use(cors());
 
 const ticketmasterApiKey = process.env.TICKETMASTER_API_KEY;
+const googleApiKey = process.env.GOOGLE_API_KEY;
 
 let events = [];
 let testEvents = [];
@@ -47,14 +48,25 @@ app.get("/api/events", (req, res) => {
     res.json(events);
 });
 
-app.get("/api/test", async (req, res) => {
+app.get("/test/events", async (req, res) => {
     try {
-        const apiUrl = `https://www.eventbriteapi.com/v3/events/search/?location.address=46505&token=${process.env.EVENTBRITE_API_KEY}`;
+        const apiUrl = ``;
         const response = await fetch(apiUrl);
         const data = await response.json();
         res.json(data);
     } catch (error) {
         console.error("Error fetching events:", error);
+    }
+});
+
+app.get("/test/places", async (req, res) => {
+    try {
+        const apiUrl = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=41.6629,-86.2529&radius=40000&type=point_of_interest&key=${googleApiKey}`;
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        res.json(data);
+    } catch (error) {
+        console.error("Error fetching places:", error);
     }
 });
 
