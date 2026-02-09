93236b75e8f8402d9d772d96e178c9458f2eed5b
Kamalani DeFreitas <109827214+kdefreitas1@users.noreply.github.com>
2026-02-09 11:20:10 -0500

    Working on adding cron

--- ticketmaster-worker/src/index.js
+++ ticketmaster-worker/src/index.js
@@ -14,52 +14,51 @@ app.use((req, res, next) => {
 	if (req.method === "OPTIONS") return res.status(204).end();
   		next();
 });
-const cron = require("node-cron");
 
 const ticketmasterApiKey = env.TICKETMASTER_API_KEY;
 
 let events = [];
-let lastUpdated = null;
+async function fetchTicketmasterEvents() {
+    try {
+        const apiUrl = `https://app.ticketmaster.com/discovery/v2/events.json?geoPoint=dp6tj&radius=40&unit=miles&size=150&sort=date,asc&apikey=${ticketmasterApiKey}`;
+        const response = await fetch(apiUrl);
+        const data = await response.json();
 
-async function fetchEvents() {
-	try {
-		const apiUrl = `https://app.ticketmaster.com/discovery/v2/events.json?geoPoint=dp6tj&radius=40&unit=miles&size=150&sort=date,asc&apikey=${ticketmasterApiKey}`;
-		const response = await fetch(apiUrl);
-		const data = await response.json();
-		console.log(data);
-		const eventImages = data._embedded.events.map(event => {
-			let found = event.images?.find(img => img.url.includes("SOURCE"))
-			|| event.images?.find(img => img.url.includes("LARGE"))
-			|| event.images?.[0]?.url;
+        const eventImages = data._embedded.events.map(event => {
+            let found = event.images?.find(img => img.url.includes("SOURCE"))
+            || event.images?.find(img => img.url.includes("LARGE"))
+            || event.images?.[0]?.url;
 
-			return found?.url;
-		});
+            return found?.url;
+        });
 
-		events = data._embedded.events.map((event, i) => ({
-			name: event.name,
-			date: event.dates.start.localDate,
-			eventType: event.classifications?.[0]?.segment?.name,
-			url: event?.url,
-			info: event?.info,
-			image: eventImages[i],
-			venue: event._embedded?.venues?.[0]?.name,
-			city: event._embedded?.venues?.[0]?.city?.name,
-		}));
+        events = data._embedded.events.map((event, i) => ({
+            name: event.name,
+            date: event.dates.start.localDate,
+            eventType: event.classifications?.[0]?.segment?.name,
+            url: event?.url,
+            info: event?.info,
+            image: eventImages[i],
+            venue: event._embedded?.venues?.[0]?.name,
+            city: event._embedded?.venues?.[0]?.city?.name,
+        }));
 
-	} catch (error) {
-		console.error("Error fetching events:", error);
-	}
-	res.json(events);
+    } catch (error) {
+        console.error("Error fetching events:", error);
+    }
 }
 
-fetchEvents();
-cron.schedule("0 */24 * * *", fetchEvents);
 
-app.get("/api/events", async (req, res) => {
+app.get("/api/events", async (req, res) => {;
 	res.json(events);
 });
 
 
 app.listen(3000);
 
-export default httpServerHandler({ port: 3000 });
+export default {
+	server: httpServerHandler({ port: 3000 }),
+	async scheduled(controller, env, ctx) {
+    	console.log("cron processed");
+	},
+};
