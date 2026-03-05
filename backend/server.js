8418d2ab1ecb3a74a2d4b7f6e9f3c87ba7fd5282
Kamalani DeFreitas <109827214+kdefreitas1@users.noreply.github.com>
2026-03-05 09:42:17 -0500

    Parks sorts well enough
    Gets about 200 parks that should be somewhat decent.

--- backend/server.js
+++ backend/server.js
@@ -60,7 +60,7 @@ app.get("/test/events", async (req, res) => {
         let dataUrl;
         let pageNum = 1;
         let url = `https://www.eventbrite.com/d/united-states/all-events/?page=${pageNum}&bbox=-86.81387816523437%2C41.21951796097693%2C-85.75919066523437%2C42.189403230536186`;
-        const browser = await puppeteer.launch({headless: false});
+        const browser = await puppeteer.launch();
         const page = await browser.newPage();
         
         page.on("response", async (response) => {
@@ -106,6 +106,7 @@ app.get("/test/events", async (req, res) => {
 app.get("/test/places", async (req, res) => {
     try {
         let places = [];
+        let timestamp;
         const minLat = 41.105;
         const maxLat = 42.2654;
         const minLon = -87.063;
@@ -115,9 +116,9 @@ app.get("/test/places", async (req, res) => {
             [out:json]
             [bbox:${minLat},${minLon},${maxLat},${maxLon}];
             (
-                node["leisure"="park"]["name"!=""];
-                way["leisure"="park"]["name"!=""];
-                relation["leisure"="park"]["name"!=""];
+                node["leisure"="park"]["name"]["operator"]["access"!="private"];
+                way["leisure"="park"]["name"]["operator"]["access"!="private"];
+                relation["leisure"="park"]["name"]["operator"]["access"!="private"];
             );
             out body;
             `;
@@ -128,7 +129,16 @@ app.get("/test/places", async (req, res) => {
         });
 
         const data = await response.json();
-        res.json(data);
+        
+        places = data.elements;
+        timestamp = data.osm3s.timestamp_osm_base;
+        copyright = data.osm3s.copyright;
+
+        res.json({
+            places: places,
+            timestamp: timestamp,
+            copyright: copyright
+        });
     } catch (error) {
         console.error("Error fetching places:", error);
     }
