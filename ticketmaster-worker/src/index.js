c9040d780ea96966a08e42683deee070b263ff3e
Kamalani DeFreitas <109827214+kdefreitas1@users.noreply.github.com>
2026-04-24 09:48:14 -0400

    Updated cloudflare worker - very broken
    Added bunch of stuff to update have no idea everything is broken must fix.

--- ticketmaster-worker/src/index.js
+++ ticketmaster-worker/src/index.js
@@ -1,6 +1,7 @@
 import { env } from "cloudflare:workers";
 import { handleAsNodeRequest } from "cloudflare:node";
 import express from "express";
+import puppeteer from "@cloudflare/puppeteer";
 
 const app = express();
 app.use(express.json());
@@ -18,7 +19,10 @@ app.use((req, res, next) => {
 const ticketmasterApiKey = env.TICKETMASTER_API_KEY;
 
 let events = [];
-let eventsLoaded = false;
+let scrapedEvents = [];
+let places = [];
+let loaded = false;
+let copyright;
 let timestamp;
 
 async function fetchTicketmasterEvents() {
@@ -45,26 +49,131 @@ async function fetchTicketmasterEvents() {
             venue: event._embedded?.venues?.[0]?.name,
             city: event._embedded?.venues?.[0]?.city?.name,
         }));
- 
-        eventsLoaded = true;  
+  
         timestamp = new Date().toISOString();   
     } catch (error) {
         console.error("Error fetching events:", error);
     }
 }
 
+async function fetchEventbriteEvents() {
+    try {
+        let events = [];
+        let data = [];
+        let dataUrl;
+        let pageNum = 1;
+        let url = `https://www.eventbrite.com/d/united-states/all-events/?page=${pageNum}&bbox=-86.81387816523437%2C41.21951796097693%2C-85.75919066523437%2C42.189403230536186`;
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        
+        page.on("response", async (response) => {
+            if(response.url().includes("/api/v3/destination/events/")) {
+                dataUrl = response.url();
+            }
+        });
 
-app.get("/api/events", async (req, res) => {
-	if (!eventsLoaded) {
-		await fetchTicketmasterEvents();
-	}
+        await page.goto(url);
+
+        while (true) {
+            await page.reload();
+            if (await page.$("li.Pagination-module__search-pagination__navigation-page___-xDRL:nth-child(3) > button:nth-child(1)") !== null) {
+                await page.reload();
+                console.log(`Cycle: ${pageNum}, Url: ${dataUrl}`);
+                await page.goto(dataUrl);
+                data = JSON.parse(await page.$eval("body > pre", el => el.textContent));
+                events.push(...data.events.map(event => ({
+                    name: event?.name,
+                    date: event?.start_date,
+                    eventType: event?.tags?.[1]?.display_name,
+                    url: event?.url,
+                    image: event?.image?.image_sizes?.large,
+                    venue: event?.primary_venue?.name,
+                    city: event?.primary_venue?.address?.city,
+                })));
+                pageNum++;
+                url = `https://www.eventbrite.com/d/united-states/all-events/?page=${pageNum}&bbox=-86.81387816523437%2C41.21951796097693%2C-85.75919066523437%2C42.189403230536186`;
+                await page.goto(url);
+            } else {
+                await page.reload();
+                timestamp = new Date().toISOString();
+                break;
+            }
+        }
+
+        await browser.close();
+        scrapedEvents = events;
+        loaded = true;
+    } catch (error) {
+        console.error("Error fetching events:", error);
+    }
+}
+
+async function fetchPlaces() {
+    try {
+        const minLat = 41.105;
+        const maxLat = 42.2654;
+        const minLon = -87.063;
+        const maxLon = -85.4781;
+        
+        const query = `
+            [out:json]
+            [bbox:${minLat},${minLon},${maxLat},${maxLon}];
+            (
+                node["leisure"="park"]["name"]["operator"]["access"!="private"];
+                way["leisure"="park"]["name"]["operator"]["access"!="private"];
+                relation["leisure"="park"]["name"]["operator"]["access"!="private"];
+            );
+            out body;
+            `;
+
+        const response = await fetch("https://overpass-api.de/api/interpreter", {
+            method: "POST",
+            body: `data=${encodeURIComponent(query)}`
+        });
+
+        try {
+            const data = await response.json();
+            places = data.elements;
+            timestamp = data.osm3s.timestamp_osm_base;
+            copyright = data.osm3s.copyright;
+        } catch (error) {
+            console.log(await response.text());
+        }
+  
+    } catch (error) {
+        console.error("Error fetching places:", error);
+    }
+}
+
+app.get("/events/tm", (req, res) => {
 	res.json({
         events: events,
         timestamp: timestamp
     });
 });
 
+app.get("/events/eb", (req, res) => {
+    res.json({
+        events: scrapedEvents,
+        timestamp: timestamp
+    });
+});
+
+app.get("/places", (req, res) => {
+    res.json({
+        places: places,
+        timestamp: timestamp,
+        copyright: copyright
+    });
+});
+
 app.listen(3000);
+
+// if (!loaded) {
+//     await fetchTicketmasterEvents();
+//     await fetchPlaces();
+//     fetchEventbriteEvents();
+// }
 
 export default {
     async fetch(request) {
@@ -72,5 +181,7 @@ export default {
     },
     async scheduled(event, env, ctx) {
         ctx.waitUntil(fetchTicketmasterEvents());
+        ctx.waitUntil(fetchPlaces());
+        ctx.waitUntil(fetchEventbriteEvents());
     },
 };
